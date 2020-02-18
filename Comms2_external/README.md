57e1a73fd83dd7ac061865e016833b615fc568cb
CoderStellaJ <shuyuanj12@gmail.com>
2020-02-18 09:14:05 +0800

    updates README for running on Ultra96 client

--- Comms2_external/README.md
+++ Comms2_external/README.md
@@ -9,12 +9,29 @@
 2. run client
 3. key in secret key in server
 ```
+cd C:\Repository\CG4002-Capstone-Project\Comms2_external
 python eval_server.py 127.0.0.1 8888 6
-python eval_client.py 127.0.0.1 8888 6 cg40024002group6
+python3 eval_client.py 127.0.0.1 8888 6 cg40024002group6
 ```
+
 secret key (length is 16):
 ```
 cg40024002group6
+```
+### Run client on Ultra96
+* USB: http://192.168.3.1
+* WiFi access point: http://192.168.2.1
+
+WIFI connection
+```
+cd C:\Repository\CG4002-Capstone-Project\Comms2_external
+python eval_server.py 172.25.104.211 5050 6
+python3 eval_client.py 172.25.104.211 5050 6 cg40024002group6
+```
+Ethernet connection
+```
+python eval_server.py 192.168.2.7 5050 6
+python3 eval_client.py 192.168.2.7 5050 6 cg40024002group6
 ```
 
 ### Questions
--- Comms2_external/eval_client.py
+++ Comms2_external/eval_client.py
@@ -8,7 +8,9 @@ from Crypto import Random
 
 BLOCK_SIZE = 16
 PADDING = ' '
-#ACTIONS = ['muscle', 'weightlifting', 'shoutout', 'dumbbells', 'tornado', 'facewipe', 'pacman', 'shootingstar', 'logout']
+
+
+# ACTIONS = ['muscle', 'weightlifting', 'shoutout', 'dumbbells', 'tornado', 'facewipe', 'pacman', 'shootingstar', 'logout']
 
 class Client():
     def __init__(self, ip_addr, port_num, group_id, secret_key):
@@ -22,13 +24,11 @@ class Client():
         # self.timeout = 60
         print("client is connected!")
 
-
     def add_padding(self, plain_text):
         pad = lambda s: s + (BLOCK_SIZE - (len(s) % BLOCK_SIZE)) * PADDING
         padded_plain_text = pad(plain_text)
         print("padded_plain_text length: ", len(padded_plain_text))
         return padded_plain_text
-
 
     def encrypt_message(self, position, action, syncdelay):
         plain_text = '#' + position + '|' + action + '|' + syncdelay + '|'
@@ -41,13 +41,18 @@ class Client():
         print(type(encrypted_text))
         return encrypted_text
 
-
     def send_data(self, position, action, syncdelay):
         encrypted_text = self.encrypt_message(position, action, syncdelay)
         print("encrypted_text: ", encrypted_text)
         sent_message = encrypted_text
         print("sent_message length: ", len(sent_message))
         self.socket.sendall(sent_message)
+
+    def stop(self):
+        self.connection.close()
+        self.shutdown.set()
+        self.timer.cancel()
+
 
 def main():
     if len(sys.argv) != 5:
@@ -62,14 +67,18 @@ def main():
 
     my_client = Client(ip_addr, port_num, group_id, secret_key)
     action = ""
-    count = 0
     # test client on laptop
     time.sleep(10)
+
+    count = 0
     while action != "logout":
         my_client.send_data("1 2 3", "muscle", "1.00")
         time.sleep(2)
         count += 1
+        if(count == 50) :
+            my_client.stop()
 
 if __name__ == '__main__':
     main()
+
 
