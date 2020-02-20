f225b5d4d42cac625c5b388c6fad4990e2e3d9d7
Chocological <melvintan196@gmail.com>
2020-02-20 23:00:10 +0800

    Latest codes

--- Comms1_internal/ultra96.py
+++ Comms1_internal/ultra96.py
@@ -25,6 +25,9 @@ beetle3_data_dict = {}
 beetle1_datastring = ""
 beetle2_datastring = ""
 beetle3_datastring = ""
+beetle1_packet_count = 0
+beetle2_packet_count = 0
+beetle3_packet_count = 0
 beetle1_dataset_count = 0
 beetle2_dataset_count = 0
 beetle3_dataset_count = 0
@@ -40,9 +43,6 @@ comma_count_3 = 0
 checksum_1 = 0
 checksum_2 = 0
 checksum_3 = 0
-is_checksum_1 = False
-is_checksum_2 = False
-is_checksum_3 = False
 # clock synchronization global variables
 timestamp_string1 = ""
 timestamp_string2 = ""
@@ -50,7 +50,6 @@ timestamp_string3 = ""
 beetle1_clocksync_flag_dict = {"1C:BA:8C:1D:30:22": False}
 beetle2_clocksync_flag_dict = {}
 beetle3_clocksync_flag_dict = {}
-# [address,t1,t2,t3,t4,t1,t2,t3,t4,t1,t2,t3,t4]
 beetle1_timestamp_list = ["1C:BA:8C:1D:30:22"]
 beetle2_timestamp_list = []
 beetle3_timestamp_list = []
@@ -60,11 +59,11 @@ beetle3_timestamp_list = []
 [beetles_connection_flag_dict.update({beetle_addresses[idx]:False})
  for idx in range(len(beetle_addresses))]
 [beetle1_data_dict.update({idx: []})
- for idx in range(1, 101)]
+ for idx in range(1, 11)]
 [beetle2_data_dict.update({idx: []})
- for idx in range(1, 101)]
+ for idx in range(1, 11)]
 [beetle3_data_dict.update({idx: []})
- for idx in range(1, 101)]
+ for idx in range(1, 11)]
 
 
 class UUIDS:
@@ -94,27 +93,9 @@ class Delegate(btle.DefaultDelegate):
         global timestamp_string1
         global timestamp_string2
         global timestamp_string3
-        global beetle1_datastring
-        global beetle2_datastring
-        global beetle3_datastring
-        global float_flag_1
-        global float_flag_2
-        global float_flag_3
-        global timestamp_flag_1
-        global timestamp_flag_2
-        global timestamp_flag_3
-        global comma_count_1
-        global comma_count_2
-        global comma_count_3
-        global beetle1_dataset_count
-        global beetle2_dataset_count
-        global beetle3_dataset_count
-        global checksum_1
-        global checksum_2
-        global checksum_3
-        global is_checksum_1
-        global is_checksum_2
-        global is_checksum_3
+        global beetle1_packet_count
+        global beetle2_packet_count
+        global beetle3_packet_count
         ultra96_receiving_timestamp = time.time() * 1000
 
         for idx in range(len(beetle_addresses)):
@@ -122,12 +103,18 @@ class Delegate(btle.DefaultDelegate):
                 print("receiving data from %s" % (beetle_addresses[idx]))
                 print("data: " + data.decode('UTF-8'))
                 if beetle_addresses[idx] in beetle1_buffer_dict:
+                    if beetle1_handshake_flag_dict[beetle_addresses[idx]] is False:
+                        beetle1_packet_count += 1  # 2 packets is 1 full dataset
                     beetle1_buffer_dict[beetle_addresses[idx]
                                         ] += data.decode('UTF-8')
                 if beetle_addresses[idx] in beetle2_buffer_dict:
+                    if beetle2_handshake_flag_dict[beetle_addresses[idx]] is False:
+                        beetle2_packet_count += 1
                     beetle2_buffer_dict[beetle_addresses[idx]
                                         ] += data.decode('UTF-8')
                 if beetle_addresses[idx] in beetle3_buffer_dict:
+                    if beetle3_handshake_flag_dict[beetle_addresses[idx]] is False:
+                        beetle3_packet_count += 1
                     beetle3_buffer_dict[beetle_addresses[idx]
                                         ] += data.decode('UTF-8')
                 if beetle_addresses[idx] in beetle1_buffer_dict:
@@ -145,8 +132,8 @@ class Delegate(btle.DefaultDelegate):
                                     {beetle_addresses[idx]: False})
                                 beetle1_clocksync_flag_dict.update(
                                     {beetle_addresses[idx]: True})
-                                beetle1_buffer_dict.update(
-                                    {"1C:BA:8C:1D:30:22": ""})
+                                beetle1_buffer_dict.update(  # clear serial input buffer to get ready for data packets
+                                    {beetle_addresses[idx]: ""})
                                 print(beetle1_timestamp_list)
                                 return
                             elif char != '>':
@@ -157,40 +144,6 @@ class Delegate(btle.DefaultDelegate):
                                     timestamp_string1 = ""
                                 else:
                                     timestamp_string1 += char
-                        elif char == 'D' or beetle1_handshake_flag_dict[beetle_addresses[idx]] is False:
-                            
-                            """
-                            if char == '|' or is_checksum_1 is True: # checksum value is next
-                                is_checksum_1 = True
-                                if char != '|':
-                                    beetle1_datastring += char
-                                checksum_1 = beetle1_datastring
-                            elif char != '|' and is_checksum_1 is False:
-                                checksum_1 ^= char
-                            """
-                            """
-                            if char != 'D' and char != '.' and char != ',' and (float_flag_1 is True or timestamp_flag_1 is True):
-                                beetle1_datastring += char
-                            elif char == '.':  # integer still belongs to original floating point value
-                                beetle1_datastring += char
-                                float_flag_1 = True
-                            elif char == ',':  # next value
-                                comma_count_1 += 1
-                                if comma_count_1 == 2:  # already past timestamp value
-                                    timestamp_flag_1 = False
-                                    beetle1_data_dict[beetle1_dataset_count].append(int(beetle1_datastring))
-                                elif (comma_count_1 > 2 and comma_count_1 < 9) or (comma_count_1 > 2 and comma_count_1 < 9):
-                                    # (D,ts,acc1,acc2,acc3|D,gy1,gy2,gy3|D,ts,acc1,acc2,acc3|D,gy1,gy2,gy3)
-                                    # D,ts,acc1,acc2,acc3,gy1,gy2,gy3,D,ts,acc1,acc2,acc3,gy1,gy2,gy3
-                                    beetle1_data_dict[beetle1_dataset_count].append(float(beetle1_datastring))
-                                beetle1_datastring = ""
-                            elif char == 'D':  # start of another packet
-                                if comma_count_1 == 14:
-
-                                beetle1_dataset_count += 1
-                                timestamp_flag_1 = True
-                            """
-
                 """
                 if beetle_addresses[idx] in beetle2_buffer_dict:
                     for char in beetle2_buffer_dict[beetle_addresses[idx]]:
@@ -216,8 +169,6 @@ class Delegate(btle.DefaultDelegate):
                                         timestamp_string2 += char
                             else:
                                 pass
-                        elif char == 'D' or beetle2_handshake_flag_dict[beetle_addresses[idx]] is False:
-                            pass
                         if beetle_addresses[idx] in beetle1_buffer_dict:
                     for char in beetle3_buffer_dict[beetle_addresses[idx]]:
                         if char == 'A' or beetle3_handshake_flag_dict[beetle_addresses[idx]] is True:
@@ -242,13 +193,10 @@ class Delegate(btle.DefaultDelegate):
                                         timestamp_string3 += char
                             else:
                                 pass
-                        elif char == 'D' or beetle3_handshake_flag_dict[beetle_addresses[idx]] is False:
-                            pass
                 """
 
 
 def initHandshake(beetle_peripheral, address):
-    global beetles_connection_flag_dict
     global beetle1_timestamp_list
     global beetle2_timestamp_list
     global beetle3_timestamp_list
@@ -265,8 +213,6 @@ def initHandshake(beetle_peripheral, address):
                     if address == beetle1_timestamp_list[0]:
                         beetle1_timestamp_list.append(
                             ultra96_sending_timestamp)
-                        beetles_connection_flag_dict.update(
-                            {address: True})
                     # uncomment below lines if testing all beetles
                     """elif address == beetle2_timestamp_list[0]:
                         beetle2_timestamp_list.append(
@@ -283,8 +229,10 @@ def initHandshake(beetle_peripheral, address):
                                 print("handshake succeeded with %s" % (
                                     address))
                                 # function for time calibration
-                                beetle1_clock_offset = calculate_clock_offset(beetle1_timestamp_list)
-                                print("beetle1 clock offset: ", beetle1_clock_offset)
+                                beetle1_clock_offset = calculate_clock_offset(
+                                    beetle1_timestamp_list)
+                                print("beetle1 clock offset: ",
+                                      beetle1_clock_offset)
                                 break
                             else:
                                 continue
@@ -327,6 +275,8 @@ def establish_connection(address):
                     beetle_peripheral.withDelegate(beetle_peri_delegate)
                     initHandshake(beetle_peripheral, address)
                     print("Connected to %s" % (address))
+                    beetles_connection_flag_dict.update(
+                        {address: True})
             break
         except:
             print("failed to connect to %s" % (address))
@@ -350,33 +300,94 @@ def reestablish_connection(beetle_peri, address):
 
 
 def getBeetleData(beetle_peri):
+    global beetle1_packet_count
+    global beetle2_packet_count
+    global beetle3_packet_count
+    global beetle1_dataset_count
+    global beetle2_dataset_count
+    global beetle3_dataset_count
     while True:
         try:
             if beetle_peri.waitForNotifications(20):
                 print("getting data...")
                 # if number of datasets received from all beetles exceed expectation
                 if beetle_peri.addr == "1C:BA:8C:1D:30:22":
-                    if len(beetle1_data_dict) >= 100:
+                    if beetle1_packet_count >= 20:
+                        print("sufficient datasets received. Processing data now")
+                        beetle1_packet_count = 0  # reset for next dance move
+                        beetle1_dataset_count = 0  # reset for next dance move
                         return
                 """
                 elif beetle_peri.addr == "1C:BA:8C:1D:30:22":
-                    if len(beetle2_data_dict) >= 100:
+                    if beetle2_packet_count >= 20:
+                        beetle2_packet_count = 0
+                        beetle2_dataset_count = 0
                         return
                 elif beetle_peri.addr == "1C:BA:8C:1D:30:22":
-                    if len(beetle3_data_dict) >= 100:
+                    if beetle3_packet_count >= 20:
+                        beetle3_packet_count = 0
+                        beetle3_dataset_count = 0
                         return
                 """
                 continue
         except Exception as e:
-            print(e)
-            """
-            try:
-                beetle_peri.disconnect()
-            except:
-                pass
-            """
             print("disconnecting beetle_peri: %s" % (beetle_peri.addr))
             reestablish_connection(beetle_peri, beetle_peri.addr)
+
+
+def processData():
+    global beetle1_buffer_dict
+    global float_flag_1
+    global timestamp_flag_1
+    global beetle1_datastring
+    global checksum_1
+    global comma_count_1
+    global beetle1_data_dict
+    global beetle1_dataset_count
+    for char in beetle1_buffer_dict["1C:BA:8C:1D:30:22"]:
+        # dummy dataset received by serial input: D1234,12.23,15.34,17.26|13>
+        if char == 'D':  # start of new dataset
+            beetle1_dataset_count += 1
+            timestamp_flag_1 = True
+            checksum_1 ^= ord(char)
+        if char != 'D' and char != '.' and char != ',' and char != '|' and char != '>' and (float_flag_1 is True or timestamp_flag_1 is True):
+            beetle1_datastring += char
+            checksum_1 ^= ord(char)
+        elif char == '.':  # integer still belongs to original floating point value
+            beetle1_datastring += char
+            checksum_1 ^= ord(char)
+        elif char == ',':  # next value
+            comma_count_1 += 1
+            checksum_1 ^= ord(char)
+            if comma_count_1 == 1:  # already past timestamp value
+                timestamp_flag_1 = False
+                beetle1_data_dict[beetle1_dataset_count].append(
+                    int(beetle1_datastring))
+                float_flag_1 = True
+            else:
+                beetle1_data_dict[beetle1_dataset_count].append(
+                    float(beetle1_datastring))
+            beetle1_datastring = ""
+        elif char == '>':  # end of current dataset
+            print("checksum_1: %i" % (checksum_1))
+            print("beetle1_datastring: %i" % (int(beetle1_datastring)))
+            # received dataset is invalid; drop the dataset from data dictionary
+            if checksum_1 != int(beetle1_datastring):
+                del beetle1_data_dict[beetle1_dataset_count]
+            beetle1_datastring = ""  # reset datastring to prepare for next dataset
+            checksum_1 = 0  # reset checksum to prepare for next dataset
+            comma_count_1 = 0
+        elif char == '|' or (float_flag_1 is False and timestamp_flag_1 is False):
+            if float_flag_1 is True:
+                beetle1_data_dict[beetle1_dataset_count].append(
+                    float(beetle1_datastring))
+                beetle1_datastring = ""  # clear datastring to prepare take in checksum from beetle
+                float_flag_1 = False
+            elif char != '|' and char != '>':
+                beetle1_datastring += char
+
+    # remember to return the data dicts here as they wont get updated at global level!!
+    return beetle1_data_dict
 
 
 """def executeMachineLearning():"""
@@ -391,17 +402,24 @@ if __name__ == '__main__':
         connection_futures = {connection_executor.submit(
             establish_connection, address): address for address in beetle_addresses}
     connection_executor.shutdown(wait=True)
-
     while True:
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as data_executor:
             receive_data_futures = {data_executor.submit(
                 getBeetleData, beetle): beetle for beetle in global_beetle_periphs}
         data_executor.shutdown(wait=True)
+        process_data_future = futures.ProcessPoolExecutor(
+            max_workers=os.cpu_count())
+        data_process = process_data_future.submit(processData)
+        # calling result() on future instances wait for the futures to finish running before proceeding
+        # remember to assign return value from result() to global data dicts!!
+        beetle1_data_dict = data_process.result()
+        print(beetle1_data_dict)
+        [beetle1_data_dict.update({idx: []})
+         for idx in range(1, 11)]
         """
         ml_future = futures.ProcessPoolExecutor(max_workers=os.cpu_count())
         ml_process = ml_future.submit(executeMachineLearning)
-        with future in concurrent.futures.as_completed(ml_process):
-            ml_future.shutdown(wait=True)
-            send_results = futures.ProcessPoolExecutor(max_workers=os.cpu_count())
-            send_results_process = send_results.submit(send_results_to_servers)
+        ml_process.result()
+        send_results = futures.ProcessPoolExecutor(max_workers=os.cpu_count())
+        send_results_process = send_results.submit(send_results_to_servers)
         """
