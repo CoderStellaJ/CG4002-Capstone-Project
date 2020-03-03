b4cd599cc5d27c0022ef5dde4f44a382c6e96518
Geraldcdx <geraldcdx5@gmail.com>
2020-03-03 15:54:05 +0800

    Added UI changes

--- Sw2_dashboard/GUI.py
+++ Sw2_dashboard/GUI.py
@@ -1,5 +1,5 @@
 #Testing for real time display
-#https://community.plot.ly/t/high-cpu-in-the-browser-and-python/17073/6
+#https://community.plot.ly/t/high-cpu-in-the-browser-and-python/17073/6 -> Hacking a method to speed up performance
 import dash
 from dash import no_update
 from dash.dependencies import Output, Input
@@ -9,14 +9,13 @@ import plotly
 import random
 import plotly.graph_objs as go
 from collections import deque
-
-from dbAPI import getLastRow
-
+from dbAPI import getLastRow #This is my API
 import dash_bootstrap_components as dbc
 from dash.exceptions import PreventUpdate
 
+#Represents the deques that will loaded into the graphs
 
-
+#The deque that is used for the x-axis
 X = deque(maxlen=10)
 X.append(1)
 
@@ -43,49 +42,52 @@ Roll3.append(1)
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+#Prevents some callback exceptions from slowing down the application
 app.config['suppress_callback_exceptions'] = True
 def main():
     init_layout(1_000)
     app.run_server(host='127.0.0.1')
 
+#Creates a layber that will store the components to be rendered
 def init_layout(refresh_interval):
         #Defines a graph
     app.layout = html.Div([
-        dbc.Row([
+    dbc.Row([
 
-            dbc.Col([html.H5('Dancer 1'),
-                    html.Div(children=html.Div(id='graphsD1', className = "row")),
-                dcc.Interval(
-                    id='graph-update',
-                    interval= 1000,
-                    n_intervals=0
-                    ),
-                     html.Div(style={'font-size':'80%'},id='Dancer1GT'),
-                     html.Div(style={'font-size':'80%'},id='Dancer1Output')
-                     ]),
+        dbc.Col([html.H5('Dancer 1'),
+                 html.Div(style={'font-size':'80%', 'background': 'white'},id='Dancer1GT'),
+                 html.Div(style={'font-size':'80%'},id='Dancer1Output'),
+                 html.Div(children=html.Div(id='graphsD1', className = "row")),
+            dcc.Interval(
+                id='graph-update',
+                interval= 1000,
+                n_intervals=0
+                )
+                ]),
 
-            dbc.Col([html.H5('Dancer 2'),
-                     html.Div(children=html.Div(id='graphsD2', className = "row")),
-                     html.Div(style={'font-size':'80%'},id='Dancer2GT'),
-                     html.Div(style={'font-size':'80%'},id='Dancer2Output')
-                     ]),
+        dbc.Col([html.H5('Dancer 2'),
+                 html.Div(style={'font-size':'80%'},id='Dancer2GT'),
+                 html.Div(style={'font-size':'80%'},id='Dancer2Output'),
+                 html.Div(children=html.Div(id='graphsD2', className = "row"))
+                 ]),
 
-            dbc.Col([html.H5('Dancer 3'),
-                     html.Div(children=html.Div(id='graphsD3', className = "row")),
-                     html.Div(style={'font-size':'80%'},id='Dancer3GT'),
-                     html.Div(style={'font-size':'80%'},id='Dancer3Output')
-                     ])
-                    #style={'float': 'left', 'margin': "5px 10px 10px 5px"}
-                        #Figure out a way to make the dancer thing remian
+        dbc.Col([html.H5('Dancer 3'),
+                 html.Div(style={'font-size':'80%'},id='Dancer3GT'),
+                 html.Div(style={'font-size':'80%'},id='Dancer3Output'),
+                 html.Div(children=html.Div(id='graphsD3', className = "row")),
+                 ])
+                #style={'float': 'left', 'margin': "5px 10px 10px 5px"}
+                    #Figure out a way to make the dancer thing remian
 
-            ]),
+        ]),
 
-        ], className="container",style={'width':'98%','margin-left':10,'margin-right':10,'max-width':5000000}
+    ], className="container",style={'width':'98%','margin-left':10,'margin-right':10,'max-width':5000000}
 
-        )
+    )
 
 
-#Data dictionary to print out a value of
+#Data dictionary to associate a string with the revevant dictionary of deques
 data_dict = {
 "Yaw1":Yaw1,
 "Pitch1": Pitch1,
@@ -110,6 +112,7 @@ def ML(lastRow):
         returnStr = "transition"
     return returnStr
 
+#Function that updates and animates all the graphs and the different changes in colour and text
 @app.callback(
     [dash.dependencies.Output('graphsD1','children'),
      dash.dependencies.Output('graphsD2','children'),
@@ -119,30 +122,45 @@ def ML(lastRow):
      dash.dependencies.Output('Dancer2Output','children'),
      dash.dependencies.Output('Dancer2GT','children'),
      dash.dependencies.Output('Dancer3Output','children'),
-     dash.dependencies.Output('Dancer3GT','children')
+     dash.dependencies.Output('Dancer3GT','children'),
+     dash.dependencies.Output('Dancer1Output','style'),
+     dash.dependencies.Output('Dancer2Output','style'),
+     dash.dependencies.Output('Dancer3Output','style'),
+     dash.dependencies.Output('Dancer1GT','style'),
+     dash.dependencies.Output('Dancer2GT','style'),
+     dash.dependencies.Output('Dancer3GT','style'),
     ],
     [dash.dependencies.Input('graph-update', 'n_intervals')]
     )
 def update_graph(n):
+    #Stops updates when the graph is intitally loading, this increases performance
     if n is None:
         raise PreventUpdate
+
+    #To be used to display graph for Dancer 1
     graphs = []
+    #Dancer 2
     graphs2 = []
+    #Dancer 3
     graphs3 = []
+    #A list of data names that are needed to update
     data_names = ['Yaw1', 'Pitch1', 'Roll1', 'Yaw2', 'Pitch2', 'Roll2', 'Yaw3', 'Pitch3', 'Roll3']
 
+    #Functions to query the database for the last saved input to PostgreSQL
     lastRow1 = getLastRow("Dancer1")
     lastRow2 = getLastRow("Dancer2")
     lastRow3 = getLastRow("Dancer3")
 
+    #Returns the Machine learning output to be displayed on the dashboard
     returnStr1 = ML(lastRow1)
     returnStr2 = ML(lastRow2)
     returnStr3 = ML(lastRow3)
 
 
-    #Append the timing for all graphs
+    #Increase the value of the X-axis by 1 for all graphs
     X.append(X[-1]+1)
 
+    #Iterate through all the data to append to the relevant deque that stores each y-axis value
     for data_name in data_names:
         if (data_name == 'Yaw1'):
             Yaw1.append(lastRow1[1])
@@ -171,16 +189,16 @@ def update_graph(n):
             mode= 'lines'
             )
 
-        #get last digit of the data_name to correspond to the dancer
+        #get last digit of the data_name to correspond to the dancer, e.g. Dancer 1/2/3
         dancerNo = data_name[-1]
 
-        #Add the graph to the graphs
+        #Add the graph to the graphs array for Dancer 1
         if(dancerNo == '1'):
             graphs.append(dbc.Col(html.Div(dcc.Graph(
                 id=data_name,
                 animate=True,
                 figure={'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
-                                                            yaxis=dict(range=[0,5]),
+                                                            yaxis=dict(range=[0,6]),
                                                             #yaxis=dict(range=[min(data_dict[data_name]),max(data_dict[data_name])]),
                                                             margin={'l':10,'r':1,'t':30,'b':30},
                                                             title='{}'.format(data_name),
@@ -190,13 +208,13 @@ def update_graph(n):
                                                             )}
             ))))
 
-        #add graph to graph2
+        #Add graph to graph2 array for Dancer 2
         elif(dancerNo == '2'):
             graphs2.append(dbc.Col(html.Div(dcc.Graph(
                 id=data_name,
                 animate=True,
                 figure={'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
-                                                            yaxis=dict(range=[0,5]),
+                                                            yaxis=dict(range=[0,6]),
                                                             margin={'l':10,'r':1,'t':30,'b':30},
                                                             title='{}'.format(data_name),
                                                             font=dict(family='Courier New, monospace', size=12, color='#7f7f7f'),
@@ -205,13 +223,13 @@ def update_graph(n):
                                                             )}
 
             ))))
-        #add graph to graph3
+        #Add graph to graph3 array for Dancer 3
         elif(dancerNo == '3'):
             graphs3.append(dbc.Col(html.Div(dcc.Graph(
                 id=data_name,
                 animate=True,
                 figure={'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
-                                                            yaxis=dict(range=[0,5]),
+                                                            yaxis=dict(range=[0,6]),
                                                             margin={'l':10,'r':1,'t':30,'b':30},
                                                             title='{}'.format(data_name),
                                                             font=dict(family='Courier New, monospace', size=12, color='#7f7f7f'),
@@ -231,7 +249,13 @@ def update_graph(n):
             "Dance Move Executed: " + returnStr2,
             "Ground Truth: " + lastRow2[0],
             "Dance Move Executed: " + returnStr3,
-            "Ground Truth: " + lastRow3[0]
+            "Ground Truth: " + lastRow3[0],
+            {'background':'springgreen'} if (n%2) else {'background': 'white'},
+            {'background':'springgreen'} if (n%2) else {'background': 'white'},
+            {'background':'springgreen'} if (n%2) else {'background': 'white'},
+            {'background':'red'} if (n%2) else {'background': 'white'},
+            {'background':'red'} if (n%2) else {'background': 'white'},
+            {'background':'red'} if (n%2) else {'background': 'white'},
             ]
 
 
