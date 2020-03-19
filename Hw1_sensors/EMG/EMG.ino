8204fa5fb7863af4b2b2b102d3bb529ffc49aa51
YiSheng <e0176969@u.nus.edu>
2020-03-19 23:55:48 +0800

    Update EMG.ino
    Extracted 3 features in time domain, mean amplitude, max amplitude and also root mean square value of the amplitude

--- Hw1_sensors/EMG/EMG.ino
+++ Hw1_sensors/EMG/EMG.ino
@@ -7,6 +7,12 @@
   This example code is in the public domain.
 */
 
+unsigned long long totalSensorValue = 0;
+unsigned long long meanSquaredValue = 0;
+float meanAmplitude = 0;
+float rmsAmplitude = 0;
+int maxAmplitude = -1;
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -15,10 +21,34 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
+  
+  // Collect 200 samples at the frequency of 1kHz, 200ms window period
+  for (int i = 0; i < 200; i++) {
+    int sensorValue = analogRead(A0);
+
+    // Compute the sum of all samples collected
+    totalSensorValue += sensorValue;
+
+    // Compute the sum of the squared value
+    meanSquaredValue += (sensorValue * sensorValue);
+
+    // Gather the largest amplitude of the EMG signal
+    if (sensorValue > maxAmplitude) {
+      maxAmplitude = sensorValue;
+    }
+    
+    // Delay for a period of 1ms such that the frequency is 1kHz
+    delay(1);
+  }
+
+  // Computing the mean amplitude value
+  meanAmplitude = totalSensorValue / 200.0;
+
+  // Computing the root mean square of the amplitude
+  meanSquaredValue = meanSquaredValue / 200.0;
+  rmsAmplitude = sqrt(meanSquaredValue);
+
+  Serial.println(maxAmplitude);
+  Serial.println(meanAmplitude);
+  Serial.println(rmsAmplitude);
 }
